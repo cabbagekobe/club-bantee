38d4ec81c3668e4da19480381332239fe18c95fb
Keisuke Kobe <circlekdeath@gmail.com>
2013-01-22 02:49:16 +0900

    Site updated to 4021a39

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-(function(){}).call(this);
+$(function(){$("a[href^=#]").click(function(){var e=$(this).attr("href"),t=$(e).offset().top;return $("html:not(:animated),body:not(:animated)").animate({scrollTop:t-0},700),!1})});
